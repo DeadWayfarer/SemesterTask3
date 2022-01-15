9e12fbcd97ffad13a72fb5d0df8daac65b1eef26
Danil <37774058+DeadWayfarer@users.noreply.github.com>
2022-01-15 11:39:46 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+string line;
+string result = "";
+while ((line = Console.ReadLine()) != "")
+{
+    var data = line.Split(" ");
+    var pupil = data[0];
+    var marks = data.Skip(1)
+                    .Select(x => int.Parse(x));
+    var avgMark = marks.Average();
+    avgMark = Math.Round(avgMark, 2);
+
+    result += $"{pupil} ср. оценка - {avgMark}\n";
+}
+Console.WriteLine(result);
